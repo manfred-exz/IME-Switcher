a377f6970150f0bcd8ccd248f14ad621d0fb3e7a
manfred_exz <manfred.exz@gmail.com>
2024-08-01 21:52:34 +0800

    more shortcut

--- ime_switcher.py
+++ ime_switcher.py
@@ -2,8 +2,6 @@ import ctypes
 import logging
 import os
 import sys
-import threading
-import time
 from ctypes import wintypes
 from datetime import datetime
 
@@ -15,7 +13,7 @@ from infi.systray import SysTrayIcon
 
 IMC_GETOPENSTATUS = 0x0005
 IMC_SETOPENSTATUS = 0x0006
-user32 = ctypes.WinDLL('user32', use_last_error=True)
+user32 = ctypes.windll.user32
 imm32 = ctypes.WinDLL('imm32', use_last_error=True)
 LANG_ID_CHINESE = '0804'
 LANG_ID_ENGLISH = '0409'
@@ -35,22 +33,22 @@ def setup_logger():
     logger = logging.getLogger('ime_switcher')
     logger.setLevel(logging.DEBUG)
 
-    log_directory = os.path.join(get_executable_directory(), 'logs')
-    if not os.path.exists(log_directory):
-        os.makedirs(log_directory)
-
-    current_date = datetime.now().strftime('%Y-%m-%d')
-    log_file = os.path.join(log_directory, f'{current_date}.log')
-    file_handler = logging.FileHandler(log_file, encoding='utf-8')
-    file_handler.setLevel(logging.DEBUG)
-
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    file_handler.setFormatter(formatter)
+    # log_directory = os.path.join(get_executable_directory(), 'logs')
+    # if not os.path.exists(log_directory):
+    #     os.makedirs(log_directory)
+    #
+    # current_date = datetime.now().strftime('%Y-%m-%d')
+    # log_file = os.path.join(log_directory, f'{current_date}.log')
+    # file_handler = logging.FileHandler(log_file, encoding='utf-8')
+    # file_handler.setLevel(logging.DEBUG)
+    #
+    # formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    # file_handler.setFormatter(formatter)
 
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.DEBUG)
 
-    logger.addHandler(file_handler)
+    # logger.addHandler(file_handler)
     logger.addHandler(console_handler)
     return logger
 
@@ -121,65 +119,81 @@ def on_switch_chinese():
     logger.info(f'{title}: switched to CHINESE')
 
 
-class HotkeyListener:
-    def __init__(self):
-        # Define virtual key codes
-        VK_SHIFT = win32con.VK_SHIFT
-        VK_CONTROL = win32con.VK_CONTROL
-        VK_MENU = win32con.VK_MENU  # ALT key
-        VK_BACKSLASH = 0xDC
-        VK_COMMA = 0xBC
-        VK_SLASH = 0xBF
+class HotKeyTrigger:
+    def register_hotkey(self, hwnd, id, modifiers, vk):
+        prototype = ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.wintypes.HWND, ctypes.c_int, ctypes.c_uint, ctypes.c_uint)
+        paramflags = (1, "hWnd", 0), (1, "id", 0), (1, "fsModifiers", 0), (1, "vk", 0)
+        RegisterHotKey = prototype(("RegisterHotKey", user32), paramflags)
+
+        return RegisterHotKey(hWnd=hwnd, id=id, fsModifiers=modifiers, vk=vk)
+
+    def unregister_hotkey(self, hwnd, id):
+        prototype = ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.wintypes.HWND, ctypes.c_int)
+        paramflags = (1, "hWnd", 0), (1, "id", 0)
+        UnregisterHotKey = prototype(("UnregisterHotKey", user32), paramflags)
+
+        return UnregisterHotKey(hWnd=hwnd, id=id)
+
+    def create_window(self):
+        wc = win32gui.WNDCLASS()
+        wc.lpfnWndProc = self.process_message
+        wc.lpszClassName = "GlobalHotkeyWindow"
+        hinst = win32gui.GetModuleHandle(None)
+        wc.hInstance = hinst
+        class_atom = win32gui.RegisterClass(wc)
+        return win32gui.CreateWindow(class_atom, "Global Hotkey Window", 0, 0, 0, 0, 0, 0, 0, hinst, None)
+
+    def process_message(self, hwnd, msg, wparam, lparam):
+        if msg == win32con.WM_HOTKEY:
+            hotkey_id = wparam
+            if hotkey_id == 1:
+                on_switch_english()
+            elif hotkey_id == 2:
+                on_switch_chinese()
+            elif hotkey_id == 3:
+                on_toggle_en_cn()
+
+            # Add more conditions for other hotkeys
+        return win32gui.DefWindowProc(hwnd, msg, wparam, lparam)
+
+    def start(self):
+        hwnd = self.create_window()
+
         VK_LBRACKET = 0xDB
         VK_RBRACKET = 0xDD
-        VK_F12 = win32con.VK_F12
+        VK_BACKSLASH = 0xDC
 
-        self.HOTKEYS = {
-            (VK_CONTROL, VK_MENU, VK_LBRACKET): on_switch_english,
-            (VK_CONTROL, VK_MENU, VK_RBRACKET): on_switch_chinese,
-            (VK_CONTROL, VK_BACKSLASH): on_toggle_en_cn,
-        }
+        # Register multiple global hotkeys
+        hotkeys = [
+            (1, win32con.MOD_CONTROL | win32con.MOD_ALT, VK_LBRACKET),
+            (2, win32con.MOD_CONTROL | win32con.MOD_ALT, VK_RBRACKET),
+            (3, win32con.MOD_CONTROL, VK_BACKSLASH),
+            # Add more hotkeys here
+        ]
 
-    @classmethod
-    def is_key_pressed(cls, vk_code):
-        return win32api.GetAsyncKeyState(vk_code) & 0x8000 != 0
+        registered_hotkeys = []
+        for id, modifiers, vk in hotkeys:
+            if self.register_hotkey(hwnd, id, modifiers, vk):
+                print(f"Global hotkey registered: ID {id}")
+                registered_hotkeys.append(id)
+            else:
+                print(f"Failed to register global hotkey: ID {id}")
 
-    def check_hotkey_combination(self):
-        for combination, callback in self.HOTKEYS.items():
-            if all(self.is_key_pressed(key) for key in combination):
-                callback()
-                # Wait for all keys in the combination to be released
-                while any(self.is_key_pressed(key) for key in combination):
-                    time.sleep(0.01)
-                return True
-        return False
-
-    def hotkey_listener(self):
-        while True:
-            if self.check_hotkey_combination():
-                time.sleep(0.1)  # Prevent multiple triggers
-            time.sleep(0.01)  # Small sleep to reduce CPU usage
-
-    def start_hotkey_listener(self):
-        self.thread = threading.Thread(target=self.hotkey_listener, daemon=True)
-        self.thread.start()
-
-    def stop(self):
-        self.thread.join()
+        # Message loop
+        msg = ctypes.wintypes.MSG()
+        while user32.GetMessageA(ctypes.byref(msg), None, 0, 0) != 0:
+            user32.TranslateMessage(ctypes.byref(msg))
+            user32.DispatchMessageA(ctypes.byref(msg))
+        # Unregister all hotkeys when done
+        for id in registered_hotkeys:
+            self.unregister_hotkey(hwnd, id)
 
 
 if __name__ == '__main__':
+    trigger = HotKeyTrigger()
     systray = SysTrayIcon("icon.ico", "IME Switcher",
-                          on_quit=lambda _: None)
+                          on_quit=lambda _: os._exit(1))
+
     systray.start()
-
-    listener = HotkeyListener()
-    listener.start_hotkey_listener()
-
-    # Keep the main thread alive
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        pass
-    listener.stop()
+    trigger.start()
+    systray.shutdown()
