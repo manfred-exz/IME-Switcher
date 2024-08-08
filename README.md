26f5b42bc939501227c0cf6736e840db0782fb6d
manfred_exz <manfred.exz@gmail.com>
2024-08-09 00:36:24 +0800

    support temporary switch language (switch back after keyboard inactive for 2s)

--- README.md
+++ README.md
@@ -2,23 +2,16 @@
 
 如果你也使用多种语言的输入法，并且希望在中文和英语输入法间快速切换，这个工具可以帮到你。
 
-|               功能                |       快捷键        |
-|:-------------------------------:|:----------------:|
-|     切换到英文/switch to English     | `<ctrl>+<alt>+[` |
-|     切换到中文/switch to Chinese     | `<ctrl>+<alt>+]` |
-| 中英文切换快捷键/toggle Chinese-English |    `<ctrl>+\`    |
+功能：
+- 切换中英文 `<ctrl>+\`
+- 暂时切换 `<ctrl>+<shfit>+\`  (停止输入2s后自动切回)
 
-If you are using multiple(>3) input methods, but you have two main languages to work with,
-this tool can to easily switch between them with a shortcut `<ctrl>+\` (you need to modify the code for your language)
+Features:
+- Switch between Chinese and English `<ctrl>+\`
+- Temporary switch `<ctrl>+<shift>+\` (automatically switches back after 2 seconds of inactivity)
 
 ## Release
 
 ```
 python -m nuitka --standalone --windows-disable-console .\main.py
 ```
-
-## Why
-
-- 切换到微软拼音输入法时，有时会自动进入ASCII模式，导致难以快速进入中文输入模式。
-- 中文输入法的ASCII模式下，不能触发部分快捷键。
-- 如果你的CJK输入法，含有ASCII模式。那么你通常难以确定当前使用的是English键盘，还是CJK键盘的ASCII模式。
--- ime_switcher.py
+++ ime_switcher.py
@@ -1,9 +1,10 @@
+import asyncio
 import ctypes
 import logging
 import os
 import sys
-from ctypes import wintypes
-from datetime import datetime
+import time
+from ctypes import CFUNCTYPE, POINTER, c_int, c_void_p, Structure, wintypes
 
 import win32api
 import win32con
@@ -20,6 +21,16 @@ LANG_ID_ENGLISH = '0409'
 
 user32.GetWindowTextW.argtypes = [wintypes.HWND, wintypes.LPWSTR, ctypes.c_int]
 user32.GetWindowTextW.restype = ctypes.c_int
+
+
+class KBDLLHOOKSTRUCT(Structure):
+    _fields_ = [
+        ("vkCode", ctypes.wintypes.DWORD),
+        ("scanCode", ctypes.wintypes.DWORD),
+        ("flags", ctypes.wintypes.DWORD),
+        ("time", ctypes.wintypes.DWORD),
+        ("dwExtraInfo", ctypes.POINTER(ctypes.wintypes.ULONG)),
+    ]
 
 
 def get_executable_directory():
@@ -119,7 +130,23 @@ def on_switch_chinese():
     logger.info(f'{title}: switched to CHINESE')
 
 
+last_key_press_time: float = None
+
+
+async def on_temp_toggle_en_cn():
+    on_toggle_en_cn()
+    logger.info('Switching back in when key is released...')
+    while True:
+        await asyncio.sleep(0.3)
+        logger.info('checking...')
+        if last_key_press_time and time.time() - last_key_press_time > 2.:
+            break
+    on_toggle_en_cn()
+
+
 class HotKeyTrigger:
+    # HOTKEY
+
     def register_hotkey(self, hwnd, id, modifiers, vk):
         prototype = ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.wintypes.HWND, ctypes.c_int, ctypes.c_uint, ctypes.c_uint)
         paramflags = (1, "hWnd", 0), (1, "id", 0), (1, "fsModifiers", 0), (1, "vk", 0)
@@ -147,17 +174,44 @@ class HotKeyTrigger:
         if msg == win32con.WM_HOTKEY:
             hotkey_id = wparam
             if hotkey_id == 1:
-                on_switch_english()
-            elif hotkey_id == 2:
-                on_switch_chinese()
-            elif hotkey_id == 3:
                 on_toggle_en_cn()
-
-            # Add more conditions for other hotkeys
+            elif hotkey_id == 2:
+                asyncio.create_task(on_temp_toggle_en_cn())
+            elif hotkey_id == 3:
+                on_switch_english()
+            elif hotkey_id == 4:
+                on_switch_chinese()
+        elif msg == win32con.WM_USER + 1:
+            global last_key_press_time
+            last_key_press_time = time.time()
         return win32gui.DefWindowProc(hwnd, msg, wparam, lparam)
 
-    def start(self):
-        hwnd = self.create_window()
+    # ANY KEY
+    def low_level_handler(self, nCode, wParam, lParam):
+        if nCode >= 0:
+            if wParam == win32con.WM_KEYUP:
+                kb = ctypes.cast(lParam, POINTER(KBDLLHOOKSTRUCT)).contents
+                win32gui.PostMessage(self.hwnd, win32con.WM_USER + 1, kb.vkCode, 0)
+        return user32.CallNextHookEx(self.hook_id, nCode, wParam, lParam)
+
+    def install_hook(self):
+        CMPFUNC = CFUNCTYPE(c_int, c_int, c_int, POINTER(c_void_p))
+        self.hooked = CMPFUNC(self.low_level_handler)
+        self.hook_id = user32.SetWindowsHookExA(
+            win32con.WH_KEYBOARD_LL, self.hooked, None, 0
+        )
+        if not self.hook_id:
+            raise ctypes.WinError()
+
+    def uninstall_hook(self):
+        if self.hook_id:
+            user32.UnhookWindowsHookEx(self.hook_id)
+            self.hook_id = None
+
+    async def start(self):
+        self.hwnd = self.create_window()
+
+        self.install_hook()
 
         VK_LBRACKET = 0xDB
         VK_RBRACKET = 0xDD
@@ -165,35 +219,38 @@ class HotKeyTrigger:
 
         # Register multiple global hotkeys
         hotkeys = [
-            (1, win32con.MOD_CONTROL | win32con.MOD_ALT, VK_LBRACKET),
-            (2, win32con.MOD_CONTROL | win32con.MOD_ALT, VK_RBRACKET),
-            (3, win32con.MOD_CONTROL, VK_BACKSLASH),
+            (1, win32con.MOD_CONTROL, VK_BACKSLASH),
+            (2, win32con.MOD_CONTROL | win32con.MOD_SHIFT, VK_BACKSLASH),
+            # (3, win32con.MOD_CONTROL | win32con.MOD_ALT, VK_LBRACKET),
+            # (4, win32con.MOD_CONTROL | win32con.MOD_ALT, VK_RBRACKET),
+
             # Add more hotkeys here
         ]
 
         registered_hotkeys = []
         for id, modifiers, vk in hotkeys:
-            if self.register_hotkey(hwnd, id, modifiers, vk):
+            if self.register_hotkey(self.hwnd, id, modifiers, vk):
                 print(f"Global hotkey registered: ID {id}")
                 registered_hotkeys.append(id)
             else:
                 print(f"Failed to register global hotkey: ID {id}")
 
         # Message loop
-        msg = ctypes.wintypes.MSG()
-        while user32.GetMessageA(ctypes.byref(msg), None, 0, 0) != 0:
-            user32.TranslateMessage(ctypes.byref(msg))
-            user32.DispatchMessageA(ctypes.byref(msg))
-        # Unregister all hotkeys when done
-        for id in registered_hotkeys:
-            self.unregister_hotkey(hwnd, id)
+        while True:
+            await asyncio.sleep(0.05)
+            win32gui.PumpWaitingMessages()
+
+        # # Unregister all hotkeys when done
+        # for id in registered_hotkeys:
+        #     self.unregister_hotkey(hwnd, id)
+        # self.uninstall_hook()
 
 
 if __name__ == '__main__':
     trigger = HotKeyTrigger()
     systray = SysTrayIcon("icon.ico", "IME Switcher",
-                          on_quit=lambda _: os._exit(1))
-
+                          on_quit=lambda _:  os._exit(1))
     systray.start()
-    trigger.start()
+    asyncio.run(trigger.start())
+    systray.shutdown()
     systray.shutdown()
